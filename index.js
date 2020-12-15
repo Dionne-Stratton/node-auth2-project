33c29fae9d85d4cdef3f8a14beb091a615c640d8
Dionne Stratton <mylife33@hotmail.com>
2020-12-15 17:06:03 -0600

    mvp

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
+require("dotenv").config();
+
 const server = require("./api/server.js");
 
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
